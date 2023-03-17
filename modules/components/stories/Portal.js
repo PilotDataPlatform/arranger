27d99c5dee7bba9e28f72d3e7bd9e024154b5541
Derek Luu <dluu@indocresearch.org>
2023-03-17 17:33:33 -0400

    remove sqon

--- modules/components/stories/Portal.js
+++ modules/components/stories/Portal.js
@@ -114,7 +114,7 @@ const Portal = ({ style, ...props }) => {
             flex-direction: column;
           `}
         >
-          <CurrentSQON {...props} />
+          {/* <CurrentSQON {...props} /> */}
           <Table {...props} />
         </div>
       </div>
