a4552c35725121dd2eef76c07e1a14aac9762bb9
Derek Luu <dluu@indocresearch.org>
2023-09-06 09:29:41 -0400

    simple styling for login to access arranger components

--- modules/components/src/App.js
+++ modules/components/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 import { Arranger, ArrangerContextProvider, ArrangerHeader, Aggregations, Table } from './Arranger';
 import KeyCloakMiddleWare from './utils/KeyCloak/KeyCloakMiddleware';
 import { keycloak } from './utils/KeyCloak/config';
 import { StyleProvider, AVAILABLE_THEMES } from './ThemeSwitcher';
+import './App.css';
 
 const ArrangerComponents = ({ ...props }) => (
   <ArrangerContextProvider>
@@ -21,6 +24,22 @@ const ArrangerComponents = ({ ...props }) => (
   </ArrangerContextProvider>
 );
 
+const ArrangerLogin = () => (
+  <div class="arranger-login">
+    <h2>Login with Pilot credentials to access Arranger Components</h2>
+    <Button onClick={() => keycloak.login({ redirectUri: window.location.origin })}>
+      <UserOutlined
+        style={{
+          marginRight: 10,
+          strokeWidth: '30',
+          stroke: 'white',
+        }}
+      />
+      Login
+    </Button>
+  </div>
+);
+
 const App = () => {
   return (
     <KeyCloakMiddleWare>
@@ -34,9 +53,7 @@ const App = () => {
           return (
             <>
               {!keycloak?.authenticated ? (
-                <button onClick={() => keycloak.login({ redirectUri: window.location.origin })}>
-                  Login to Pilot
-                </button>
+                <ArrangerLogin />
               ) : (
                 <ArrangerComponents
                   {...{ ...props, graphqlField: 'pilotdevtestalias', projectId: 'pilotdev' }}
