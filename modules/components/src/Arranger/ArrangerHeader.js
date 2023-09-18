6f8bd5bbd9b523b711610b1b6f687b060c23ee0a
Derek Luu <dluu@indocresearch.org>
2023-09-18 14:47:30 -0400

    Update tooltip in header

--- modules/components/src/Arranger/ArrangerHeader.js
+++ modules/components/src/Arranger/ArrangerHeader.js
@@ -5,7 +5,10 @@ import { QuestionCircleOutlined } from '@ant-design/icons';
 
 import './ArrangerHeader.css';
 
-const ArrangerHeader = ({ title = 'Search', tooltipTitle = 'test tooltip' }) => (
+const DEFAULT_TOOLTIP =
+  'Use the search filters to find Project file metadata in the Green Room and Core. Click Download to download the search metadata in tab separated value (TSV) format.';
+
+const ArrangerHeader = ({ title = 'Search', tooltipTitle = DEFAULT_TOOLTIP }) => (
   <div className="arranger-header">
     <p className="arranger-header__title">
       {title}
