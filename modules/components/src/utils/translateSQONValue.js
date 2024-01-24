c3498dd2f1311d7ae548b1c7a350496da5c9fd70
Derek Luu <dluu@indocresearch.org>
2024-01-24 14:49:23 -0500

    remove formatter from adding commas to numbers in aggregate bar
    add comment

--- modules/components/src/utils/translateSQONValue.js
+++ modules/components/src/utils/translateSQONValue.js
@@ -1,8 +1,9 @@
-import formatNumber from '../utils/formatNumber';
+// keep this here incase we need to add commas to numbers in the future
+// import formatNumber from '../utils/formatNumber';
 
 export default (value) => {
   if (value === '__missing__') {
     return 'Other';
   }
-  return formatNumber(value);
+  return value;
 };
